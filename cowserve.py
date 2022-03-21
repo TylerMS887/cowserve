0d3b6b57db90e923b116900612d89d2c2c9361c8
Tyler887 <tylermageeshields@gmail.com>
2022-03-21 18:48:17 +0000

    Update cowserve.py

--- cowserve.py
+++ cowserve.py
@@ -7,19 +7,21 @@ print("             PROJECT COWSERVE - cross-platform web server\n              
 import os
 import http.server
 import socketserver
+import webbrowser
 PORT = str(arg.port)
 PORT = PORT.replace("[", "")
 PORT = PORT.replace("]", "")
 PORT = int(PORT)
 if not os.path.isfile(f"{os.getcwd()}/index.html"):
   with open(f"{os.getcwd()}/index.html", "w") as file:
-    file.write("""
+    email = input("Please enter your email: ")
+    file.write(f"""
           <title>Cowserve</title>
           <div align='center'>
           <h1>It works!</h1>
           </div>
           This page is used to test proper installation of Cowserve. You should edit this file, <code>index.html</code>, before continuing.
-          If you are a regular user of this website, and did not expect this page, contact the site's admin.
+          If you are a regular user of this website, and did not expect this page, contact the <a href='mailto:{email}'>site's admin</a>.
           <h2>About Cowserve</h2>
           <b>Cowserve</b> (also known as <b>Project Cowserve</b>) is a <a href='https://en.wikipedia.org/wiki/Free_and_open-source_software'>free and open-source</a>
           web server written in a free and open-source programming language, Python. It supports many features, including:
@@ -43,6 +45,7 @@ if not os.path.isfile(f"{os.getcwd()}/index.html"):
           If you find any bugs, please use the <a href='https://github.com/Tyler887/cowserve/issues'>issue tracker on GitHub</a>. Remember to search for
           existing issues before opening a new one.
     """)
+webbrowser.open(f"http://localhost:{str(PORT)}")
 Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Starting up at port", str(PORT) + ".")
